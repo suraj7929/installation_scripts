696911cbf517470f8830af1b771a0ce316a748d4
Sharmaranjan <110963614+Sharmaranjan@users.noreply.github.com>
2023-01-21 17:23:42 +0530

    Update install-puppet-agent.sh

--- install-puppet-agent.sh
+++ install-puppet-agent.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# Download and install the puppet agent package
-wget https://apt.puppetl
+# Update the package list
+sudo apt-get update
+
+# Install the Puppet agent
+sudo apt-get install -y puppet-agent
+
+# Configure the Puppet agent
+sudo echo -e "[agent]\nserver = ${1}\nport = ${2}\ncertname = ${3}" > /etc/puppetlabs/puppet/puppet.conf
+
+# Start the Puppet agent
+sudo systemctl start puppet
